24f89068ee4e5fd745f133c66b45bba8cd943c3a
icocoon <ihuangheqing@gmail.com>
2025-11-29 22:07:29 +0800

    放弃侧边栏

--- components/PageAside.tsx
+++ components/PageAside.tsx
@@ -1,50 +1,9 @@
-// import { type Block, type ExtendedRecordMap } from 'notion-types'
-
-// import { getPageTweet } from '@/lib/get-page-tweet'
-
-// import { PageActions } from './PageActions'
-// import { PageSocial } from './PageSocial'
-
-// export function PageAside({
-//   block,
-//   recordMap,
-//   isBlogPost
-// }: {
-//   block: Block
-//   recordMap: ExtendedRecordMap
-//   isBlogPost: boolean
-// }) {
-//   if (!block) {
-//     return null
-//   }
-
-//   // only display comments and page actions on blog post pages
-//   if (isBlogPost) {
-//     const tweet = getPageTweet(block, recordMap)
-//     if (!tweet) {
-//       return null
-//     }
-
-//     return <PageActions tweet={tweet} />
-//   }
-
-//   return <PageSocial />
-// }
-
-
-import * as React from 'react'
-import dynamic from 'next/dynamic'
 import { type Block, type ExtendedRecordMap } from 'notion-types'
+
 import { getPageTweet } from '@/lib/get-page-tweet'
+
 import { PageActions } from './PageActions'
 import { PageSocial } from './PageSocial'
-
-// 使用动态导入，并添加 @ts-ignore 忽略 TypeScript 的路径报错
-const TableOfContents = dynamic(() =>
-  // @ts-ignore: 忽略第三方库的类型定义路径问题
-  import('react-notion-x/third-party/table-of-contents').then((m) => m.TableOfContents),
-  { ssr: false }
-)
 
 export function PageAside({
   block,
@@ -59,18 +18,14 @@ export function PageAside({
     return null
   }
 
-  // 如果是博客文章
+  // only display comments and page actions on blog post pages
   if (isBlogPost) {
     const tweet = getPageTweet(block, recordMap)
-    
-    return (
-      <div className='notion-aside-custom'>
-        {tweet && <PageActions tweet={tweet} />}
-        
-        {/* 修正参数：将 block.id 作为 pageId 传递给目录组件 */}
-        <TableOfContents recordMap={recordMap} pageId={block.id} />
-      </div>
-    )
+    if (!tweet) {
+      return null
+    }
+
+    return <PageActions tweet={tweet} />
   }
 
   return <PageSocial />
